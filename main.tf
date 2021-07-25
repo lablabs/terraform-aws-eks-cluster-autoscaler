55f36c367ff01631a9ef05a7fdfbe1c0f0d16dc7
eranelbaz <eranelbaz97@gmail.com>
2021-07-25 17:40:25 +0300

    change structure

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 data "aws_region" "current" {}
 
 resource "helm_release" "cluster_autoscaler" {
-  depends_on = [var.mod_dependency]
+  depends_on = [
+    var.mod_dependency]
   count = var.enabled ? 1 : 0
   chart = var.helm_chart_name
   namespace = var.k8s_namespace
@@ -9,14 +10,19 @@ resource "helm_release" "cluster_autoscaler" {
   version = var.helm_chart_version
   repository = var.helm_repo_url
 
-  values     = [yamlencode({
-    "awsRegion"                                                  : data.aws_region.current.name
-    "autoDiscovery.clusterName"                                  : var.cluster_name
-    "rbac.create"                                                : true
-    "rbac.serviceAccount.create"                                 : true
-    "rbac.serviceAccount.name"                                   : var.k8s_service_account_name
-    "rbac.serviceAccount.annotations.eks.amazonaws.com/role-arn" : var.k8s_service_account_name
-  }), var.values]
+  values = [
+    yamlencode({
+      "awsRegion"                                                  : data.aws_region.current.name
+      "autoDiscovery"                                  : {
+
+        "clusterName"                                  : var.cluster_name
+      }
+      "rbac.create"                                                : true
+      "rbac.serviceAccount.create"                                 : true
+      "rbac.serviceAccount.name"                                   : var.k8s_service_account_name
+      "rbac.serviceAccount.annotations.eks.amazonaws.com/role-arn" : var.k8s_service_account_name
+    }),
+    var.values]
 
   dynamic "set" {
     for_each = var.settings
