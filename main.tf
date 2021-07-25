8e9c0d8322a2a45ff64fff73180aea55597cb47c
eranelbaz <eranelbaz97@gmail.com>
2021-07-25 16:51:16 +0300

    another yamlencode check

--- main.tf
+++ main.tf
@@ -1,27 +1,43 @@
 data "aws_region" "current" {}
 
 resource "helm_release" "cluster_autoscaler" {
-  depends_on = [var.mod_dependency]
-  count      = var.enabled ? 1 : 0
-  chart      = var.helm_chart_name
-  namespace  = var.k8s_namespace
-  name       = var.helm_release_name
-  version    = var.helm_chart_version
+  depends_on = [
+    var.mod_dependency]
+  count = var.enabled ? 1 : 0
+  chart = var.helm_chart_name
+  namespace = var.k8s_namespace
+  name = var.helm_release_name
+  version = var.helm_chart_version
   repository = var.helm_repo_url
 
-  values     = [yamlencode({
-    "awsRegion"                                                  : data.aws_region.current.name
-    "autoDiscovery.clusterName"                                  : var.cluster_name
-    "rbac.create"                                                : true
-    "rbac.serviceAccount.create"                                 : true
-    "rbac.serviceAccount.name"                                   : var.k8s_service_account_name
-    "rbac.serviceAccount.annotations.eks.amazonaws.com/role-arn" : var.k8s_service_account_name
-  }), var.values]
+  values = [
+    yamlencode({
+      "awsRegion": data.aws_region.current.name,
+      "autoDiscovery": {
+        "clusterName": var.cluster_name
+      },
+      "rbac": {
+        "create": true,
+        "serviceAccount": {
+          "create": true,
+          "name": var.k8s_service_account_name,
+          "annotations" : {
+
+            "eks": {
+              "amazonaws": {
+                "com/role-arn": var.k8s_service_account_name
+              }
+            }
+          }
+        }
+      }
+    }),
+    var.values]
 
   dynamic "set" {
     for_each = var.settings
     content {
-      name  = set.key
+      name = set.key
       value = set.value
     }
   }
