be96e05959694be21b796c8c4d7126137974b64a
Saurabh Taneja <99720728+saurabh-paystack@users.noreply.github.com>
2022-04-27 14:40:55 +0400

    Update iam.tf
    Updating IAM permissions to fix https://github.com/kubernetes/autoscaler/issues/3216 error

--- iam.tf
+++ iam.tf
@@ -11,7 +11,8 @@ data "aws_iam_policy_document" "cluster_autoscaler" {
       "autoscaling:DescribeTags",
       "autoscaling:SetDesiredCapacity",
       "autoscaling:TerminateInstanceInAutoScalingGroup",
-      "ec2:DescribeLaunchTemplateVersions"
+      "ec2:DescribeLaunchTemplateVersions",
+      "ec2:DescribeInstanceTypes"
     ]
 
     resources = [
