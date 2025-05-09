b71869a83bec934eb4e0d2c978ac227ff698c3ed
Matej Hasul <matej.hasul@lablabs.io>
2025-05-09 16:25:02 +0200

    feat(examples): Add examples

---
+++ examples/basic/main.tf
@@ -0,0 +1,81 @@
+module "addon_installation_disabled" {
+  source = "../../"
+
+  enabled = false
+
+  cluster_identity_oidc_issuer     = module.eks_cluster.eks_cluster_identity_oidc_issuer
+  cluster_identity_oidc_issuer_arn = module.eks_cluster.eks_cluster_identity_oidc_issuer_arn
+}
+
+module "addon_installation_helm" {
+  source = "../../"
+
+  enabled           = true
+  argo_enabled      = false
+  argo_helm_enabled = false
+
+  cluster_identity_oidc_issuer     = module.eks_cluster.eks_cluster_identity_oidc_issuer
+  cluster_identity_oidc_issuer_arn = module.eks_cluster.eks_cluster_identity_oidc_issuer_arn
+
+  values = yamlencode({
+    # insert sample values here
+  })
+}
+
+module "addon_installation_helm_pod_identity" {
+  source = "../../"
+
+  enabled           = true
+  argo_enabled      = false
+  argo_helm_enabled = false
+
+  cluster_name = module.eks_cluster.eks_cluster_id
+
+  # Disable IRSA
+  irsa_role_create = false
+
+  # Enable pod identity
+  pod_identity_role_create = true
+
+  values = yamlencode({
+    # insert sample values here
+    clusterName = module.eks_cluster.eks_cluster_id
+  })
+}
+
+# Please, see README.md and Argo Kubernetes deployment method for implications of using Kubernetes installation method
+module "addon_installation_argo_kubernetes" {
+  source = "../../"
+
+  enabled           = true
+  argo_enabled      = true
+  argo_helm_enabled = false
+
+  cluster_identity_oidc_issuer     = module.eks_cluster.eks_cluster_identity_oidc_issuer
+  cluster_identity_oidc_issuer_arn = module.eks_cluster.eks_cluster_identity_oidc_issuer_arn
+
+  values = yamlencode({
+    # insert sample values here
+  })
+
+  argo_sync_policy = {
+    automated   = {}
+    syncOptions = ["CreateNamespace=true"]
+  }
+}
+
+module "addon_installation_argo_helm" {
+  source = "../../"
+
+  enabled           = true
+  argo_enabled      = true
+  argo_helm_enabled = true
+
+  cluster_identity_oidc_issuer     = module.eks_cluster.eks_cluster_identity_oidc_issuer
+  cluster_identity_oidc_issuer_arn = module.eks_cluster.eks_cluster_identity_oidc_issuer_arn
+
+  argo_sync_policy = {
+    automated   = {}
+    syncOptions = ["CreateNamespace=true"]
+  }
+}
