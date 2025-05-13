24dcd00a5786bf35bc0e4d1085874f6a49a96149
Matej Hasul <matej.hasul@lablabs.io>
2025-05-13 10:56:04 +0200

    fix(migration): Add moved blocks for all installation methods

--- migrations.tf
+++ migrations.tf
@@ -1,7 +1,17 @@
-# Argo
+# Installation methods
+moved {
+  from = helm_release.this
+  to   = module.addon.helm_release.this
+}
+
 moved {
   from = helm_release.argo_application
   to   = module.addon.helm_release.argo_application
+}
+
+moved {
+  from = kubernetes_manifest.this
+  to   = module.addon.kubernetes_manifest.this
 }
 
 # IRSA
