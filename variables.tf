f003e77ef3a856e0c1bf858bfa1805de85221d85
Martin Odstrčilík <martin.odstrcilik@lablabs.io>
2025-12-15 14:55:53 +0100

    chore: apply sync patch

--- variables.tf
+++ variables.tf
@@ -1,6 +1,1 @@
 # IMPORTANT: Add addon specific variables here
-variable "enabled" {
-  type        = bool
-  default     = true
-  description = "Set to false to prevent the module from creating any resources."
-}
